4a9c87005b042513c3a9cef8a266d26979336566
adityanz <aditya@adityadixit.com>
2018-04-09 13:37:57 -0700

    Example Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    private static final int MAXIMUM = 10;
+
+    public static void main(String[] args) {
+        System.out.println("Good Afternoon");
+        System.out.println("Hope you have a good day");
+        int j = 0;
+        for (int i = 0; i < MAXIMUM; i++) {
+            j = i;
+            j = myMethod(j);
+            if (j == 50) {
+                throw new MyException();
+            }
+        }
+    }
+
+    private static int myMethod (int j) {
+        int result;
+        result = j * MAXIMUM;
+        return result;
+    }
+
+    private static class MyException extends RuntimeException {
+
+    }
+
+    }
+
